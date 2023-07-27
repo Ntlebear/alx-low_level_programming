bae7a792521f8391a5e663b6dc004eea219e9790
Ntlentle Moloi <grayntle@yahoo.com>
2023-07-27 14:38:20 -0700

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,38 +1,60 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
- */
-char *cap_string(char *str)
+ * upper - capitalizes a letter if it is lowercase
+ * @c: pointer to a string
+ * Return: pointer to string
+ **/
+char *upper(char *c)
 {
-	int index = 0;
+	if (c[0] >= 'a' && c[0] <= 'z')
+		c[0] -= 32;
+	return (c);
+}
 
-	while (str[index])
+/**
+ * new_word - compares char to chars that indicate a new word
+ * @c: char to check
+ * Return: 1 if char is a word separator, 0 of not
+ **/
+int new_word(char c)
+{
+	int i = 0;
+	char *word = " \t\n,;.!?\"(){}";
+
+	while (word[i] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
+		if (c == word[i])
+			return (1);
+		i++;
 	}
 
+	return (0);
+}
+
+/**
+ * cap_string - capitalize all words in a string
+ * @str: string to modify
+ * Return: pointer to str
+ **/
+char *cap_string(char *str)
+{
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	upper(str);
+	if (str[0] == '\0')
+		return (str);
+
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (new_word(str[i - 1]) == 1)
+		{
+			upper(&str[i]);
+		}
+	}
 	return (str);
 }
